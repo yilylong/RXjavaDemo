ab59e62c42947cac5469c5fba6d38b726adfabe8
yilylong <yilylong@163.com>
2018-10-23 11:49:24 +0800

    测试sharedelement

---
+++ app/src/main/java/com/zhl/rx/activity/FloatDetailActivity.java
@@ -0,0 +1,19 @@
+package com.zhl.rx.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zhl.rx.R;
+
+/**
+ * 描述：
+ * Created by zhaohl on 2018-10-23.
+ */
+public class FloatDetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_float_detail);
+//        getWindow().setEnterTransition(new Slide(Gravity.RIGHT));
+    }
+}
--- app/src/main/java/com/zhl/rx/activity/FloatImageViewTest.java
+++ app/src/main/java/com/zhl/rx/activity/FloatImageViewTest.java
@@ -1,10 +1,16 @@
 package com.zhl.rx.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.zhl.rx.R;
@@ -18,17 +24,22 @@ public class FloatImageViewTest extends AppCompatActivity {
         setContentView(R.layout.activity_float_image_view_test);
         final FrameLayout frameLayout = findViewById(R.id.fml);
         final FloatImageView floatImageView = findViewById(R.id.floatView);
+        final ImageView userhead = findViewById(R.id.img);
+        final TextView username = findViewById(R.id.tv);
         Button button = findViewById(R.id.btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                floatImageView.setMaxWidthAndHeight(frameLayout.getWidth(),frameLayout.getHeight());
+                floatImageView.setMaxWidthAndHeight(frameLayout.getWidth(), frameLayout.getHeight());
             }
         });
         floatImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FloatImageViewTest.this,"点击浮窗广告",Toast.LENGTH_SHORT).show();
+                Toast.makeText(FloatImageViewTest.this, "点击浮窗广告", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(FloatImageViewTest.this,FloatDetailActivity.class);
+                ActivityCompat.startActivity(FloatImageViewTest.this,intent,
+                        ActivityOptionsCompat.makeSceneTransitionAnimation(FloatImageViewTest.this, Pair.create(((View)userhead),"userhead"),Pair.create(((View)username),"desc")).toBundle());
             }
         });
     }
